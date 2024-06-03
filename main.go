1718c528ae48433041ae3882b73e53b2eec72182
WilmerLeonCh <wleonch@hotmail.com>
2024-06-03 10:57:13 -0500

    feat: build and release option list - crud

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/PhotoManager/tui"
+
+func main() {
+	tui.CreateOptionList()
+}
---
+++ tui/option-list.go
@@ -0,0 +1,131 @@
+package tui
+
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+
+	"github.com/PhotoManager/utils"
+)
+
+type view int
+
+const (
+	createView view = iota
+	listSearchView
+	updateView
+	deleteView
+	optionListView
+)
+
+type tuiCursor int
+
+var choiceActions = []string{
+	tuiCursor(0): "Create",
+	tuiCursor(1): "List & Search",
+	tuiCursor(2): "Update",
+	tuiCursor(3): "Delete",
+}
+
+type MOptionList struct {
+	tuiCursor   tuiCursor
+	tuiSelected view
+	tuiCmd      func(tea.Msg) (tea.Model, tea.Cmd)
+}
+
+func CreateOptionList() {
+	bubble := tea.NewProgram(NewMOptionList())
+	utils.Must(bubble.Run())
+}
+
+func NewMOptionList() *MOptionList {
+	return &MOptionList{
+		tuiCursor:   tuiCursor(0),
+		tuiSelected: optionListView,
+		tuiCmd:      nil,
+	}
+}
+
+func (m *MOptionList) Init() tea.Cmd { return nil }
+
+func (m *MOptionList) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyTab, tea.KeyDown:
+			m.increaseViewCursor()
+		case tea.KeyShiftTab, tea.KeyUp:
+			m.decreaseViewCursor()
+		case tea.KeyEnter:
+			m.tuiSelected = view(m.tuiCursor)
+			return m.handleCmd()
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		default:
+			return m, nil
+		}
+	}
+	return m, nil
+}
+
+func (m *MOptionList) View() string {
+	return m.handleView()
+}
+
+func (m *MOptionList) increaseViewCursor() {
+	m.tuiCursor = tuiCursor((int(m.tuiCursor) + 1) % len(choiceActions))
+}
+
+func (m *MOptionList) decreaseViewCursor() {
+	m.tuiCursor--
+	if m.tuiCursor < 0 {
+		m.tuiCursor = tuiCursor(len(choiceActions) - 1)
+	}
+}
+
+func (m *MOptionList) handleView() string {
+	switch m.tuiSelected {
+	case optionListView:
+		return m.RenderOptionList()
+	case createView:
+		return "CreateView"
+	case listSearchView:
+		return "ListSearchView"
+	case updateView:
+		return "UpdateView"
+	case deleteView:
+		return "DeleteView"
+	default:
+		return ""
+	}
+}
+
+func (m *MOptionList) handleCmd() (tea.Model, tea.Cmd) {
+	switch m.tuiSelected {
+	case optionListView:
+		return m, nil
+	case createView:
+		return m, nil
+	case listSearchView:
+		return m, nil
+	case updateView:
+		return m, nil
+	case deleteView:
+		return m, nil
+	default:
+		return m, nil
+	}
+}
+
+func (m *MOptionList) RenderOptionList() string {
+	s := "What would you like to do?\n\n"
+	for i, choice := range choiceActions {
+		cursor := " "
+		if tuiCursor(i) == m.tuiCursor {
+			cursor = ">"
+		}
+		s += fmt.Sprintf("%s %s\n", cursor, choice)
+	}
+	s += "\nPress 'ctrl+c' or 'esc' to quit.\n"
+	return s
+}
